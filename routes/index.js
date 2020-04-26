3817eaf1e6bbb99f7bede105ed849e76b11fa5c3
LEvinson2504 <ldsouza2504@gmail.com>
2020-04-26 19:42:03 +0530

    added pending order page, tried to add stripe

--- routes/index.js
+++ routes/index.js
@@ -2,26 +2,32 @@ const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated } = require("../config/auth");
 const User = require('../models/User');
-const {Menu} = require('../models/User')
+const { Menu } = require('../models/User');
+
+// Set your secret key. Remember to switch to your live secret key in production!
+// See your keys here: https://dashboard.stripe.com/account/apikeys
+const stripe = require('stripe')('sk_test_xjJahMxr5HTQeV4bXDztjFGT00yvfnuQmY');
 
 //make styles public
-router.use( express.static( "public/" ) )
+router.use(express.static("public/"))
 //body parser
 router.use(express.json());
 //welcome page
 router.get('/', (req, res) => res.render('welcome'));
 
 //dashboard
-router.get('/dashboard', ensureAuthenticated, (req, res) => { 
+router.get('/dashboard', ensureAuthenticated, (req, res) => {
+    console.log(req.user);
     res.render('dashboard', {
         name: req.user.name,
         menu: req.user.menu,
+        orders: req.user.orders,
         barcode: "https://pacific-savannah-86216.herokuapp.com/menu/" + req.user._id,
     });
 
 })
 
-router.post('/dashboard', ensureAuthenticated, (req, res) => { 
+router.post('/dashboard', ensureAuthenticated, (req, res) => {
     console.log(req.body.tablenumber);
     res.render('dashboard', {
         name: req.user.name,
@@ -34,11 +40,11 @@ router.post('/dashboard', ensureAuthenticated, (req, res) => {
 router.get('/menu/:id/:table?', (req, res) => {
 
     //shorthand for let id = req.params.id
-    const {id, table} = req.params;
+    const { id, table } = req.params;
     // res.json(menuId);
     // console.log(req.params);
-    User.findOne({_id: id}, function(err, user){
-        if(err) {
+    User.findOne({ _id: id }, function (err, user) {
+        if (err) {
             res.status(500).send();
         } else {
             // console.log(user);
@@ -57,9 +63,9 @@ router.get('/menu/:id/:table?', (req, res) => {
 //Place order 
 router.post("/menu/order", express.json(), (req, res) => {
     console.log(req.body);
-    const {items, table, totalPrice} = req.body;
-    console.log( items + " was ordered on table " + table);
-    User.findOne({name: "test"}, function(err, user){
+    const { items, table, totalPrice } = req.body;
+    console.log(items + " was ordered on table " + table);
+    User.findOne({ name: "test" }, function (err, user) {
         // console.log(user.orders);
         if (err) {
             res.status(500).send();
@@ -70,8 +76,8 @@ router.post("/menu/order", express.json(), (req, res) => {
                 totalPrice: totalPrice
             })
         }
-        user.save(function(err, savedUser){
-            if(err){
+        user.save(function (err, savedUser) {
+            if (err) {
                 res.status(500).send();
             } else {
                 res.render('pendingOrders', {
@@ -81,7 +87,24 @@ router.post("/menu/order", express.json(), (req, res) => {
             }
         })
     });
-    
+
+});
+
+
+router.get('/checkout', async (req, res) => {
+    try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: 2000,
+      currency: 'inr',
+      // Verify your integration in this guide by including this parameter
+      metadata: {integration_check: 'accept_a_payment'},
+    });
+
+    const {client_secret} = paymentIntent;
+    res.render('checkout', { client_secret});
+    } catch(err) {
+        // alert("Payment failed");
+    }
 });
 
 module.exports = router;
